5d3c80102f6dcc6b18c155d107369ccf8ee0ce20
Khanh <leqkhanh0109@gmail.com>
2024-08-20 17:46:30 +0700

    update

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/ForgotPasswordActivity.java
@@ -1,4 +1,4 @@
-package com.example.ecommercemobileapp2hand.Views.Account;
+package com.example.ecommercemobileapp2hand.Views.Login;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ecommercemobileapp2hand.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
