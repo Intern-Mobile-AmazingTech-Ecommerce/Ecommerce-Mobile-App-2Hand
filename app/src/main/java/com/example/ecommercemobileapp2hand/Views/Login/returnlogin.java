e0f31a87739ab95ba5211a1e9915f2420806cdb5
khuongduong_ <134796453+khuongduong99@users.noreply.github.com>
2024-08-20 19:11:35 +0700

    returnlogin

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/returnlogin.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/returnlogin.java
@@ -1,6 +1,9 @@
 package com.example.ecommercemobileapp2hand.Views.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ecommercemobileapp2hand.R;
+import com.example.ecommercemobileapp2hand.Views.SignUp;
 
 public class returnlogin extends AppCompatActivity {
-
+    Button btnReturnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,15 @@ public class returnlogin extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnReturnLogin = findViewById(R.id.btnReturnLogin);
+        //nut Return Login
+        btnReturnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(returnlogin.this, SignUp.class);
+                startActivity(myintent);
+            }
         });
     }
 }
