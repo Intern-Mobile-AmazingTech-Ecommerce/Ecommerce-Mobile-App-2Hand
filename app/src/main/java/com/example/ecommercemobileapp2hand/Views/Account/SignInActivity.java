ca73f7cceb1e7dd79ab1c88babb04af5725da295
Khanh <leqkhanh0109@gmail.com>
2024-08-20 17:32:05 +0700

    update SignIn, SignUp UI

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Account/SignInActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Account/SignInActivity.java
@@ -1,4 +1,4 @@
-package com.example.ecommercemobileapp2hand.Views;
+package com.example.ecommercemobileapp2hand.Views.Account;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ecommercemobileapp2hand.R;
 
-public class SignUp extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Account/SignInPasswordActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Account/SignInPasswordActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ecommercemobileapp2hand.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class SignInPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_sign_in_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
