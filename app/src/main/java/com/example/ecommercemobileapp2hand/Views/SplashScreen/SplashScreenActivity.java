b79d1a7a0d42769ba7f975b86ce9b5431f6893c0
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-08-21 16:30:23 +0700

    Adjust UI

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/SplashScreen/SplashScreenActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/SplashScreen/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.ecommercemobileapp2hand.Views.SplashScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ecommercemobileapp2hand.R;
+import com.example.ecommercemobileapp2hand.Views.Login.SignInActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -17,10 +20,18 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
