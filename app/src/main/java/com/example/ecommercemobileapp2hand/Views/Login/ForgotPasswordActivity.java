9f25c95066406ab5a5fbbe186d2c682e238f2921
Đặng Hoàng Phúc <131565609+HoangPhuc0612@users.noreply.github.com>
2024-08-21 18:21:09 +0700

    ActivityChanges

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/ForgotPasswordActivity.java
@@ -1,6 +1,10 @@
 package com.example.ecommercemobileapp2hand.Views.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,8 @@ import com.example.ecommercemobileapp2hand.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    Button btnContinue_2;
+    ImageButton btnReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,27 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        sendEmailReset();
+        returnToSignInPassword();
+    }
+    private void sendEmailReset(){
+        btnContinue_2=(Button) findViewById(R.id.btnContinue_2);
+        btnContinue_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ForgotPasswordActivity.this,returnlogin.class));
+                finish();
+            }
+        });
+    }
+    private void returnToSignInPassword(){
+        btnReturn=(ImageButton) findViewById(R.id.btn_return);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
         });
     }
 }
--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/SignUpActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/SignUpActivity.java
@@ -1,6 +1,11 @@
 package com.example.ecommercemobileapp2hand.Views.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,9 @@ import com.example.ecommercemobileapp2hand.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    Button btnContinue;
+    ImageButton btnReturn;
+    TextView txtResetPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +29,39 @@ public class SignUpActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        continueCreateAccount();
+        returnToSignIn();
+        resetPassword();
+    }
+    private void continueCreateAccount(){
+        btnContinue=(Button) findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(SignUpActivity.this,));
+                //finish();
+            }
+        });
+    }
+    private void returnToSignIn(){
+        btnReturn=(ImageButton) findViewById(R.id.btn_return);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+                finish();
+            }
+        });
+    }
+    private void resetPassword(){
+        txtResetPassword=(TextView) findViewById(R.id.dont_have_a);
+        txtResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, ForgotPasswordActivity.class));
+                finish();
+            }
         });
     }
 }
