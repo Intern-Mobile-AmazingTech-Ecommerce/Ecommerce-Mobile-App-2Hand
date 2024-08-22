9330609f2cd1f2d4e724ee91cc411dda2b5b3ff2
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-08-22 15:52:40 +0700

    50% product page

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/ProductPage/ProductPage.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/ProductPage/ProductPage.java
@@ -7,11 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ecommercemobileapp2hand.R;
+import com.example.ecommercemobileapp2hand.Views.ProductPage.Adapters.RecycleProductImageAdapter;
+
+import java.util.ArrayList;
 
 public class ProductPage extends AppCompatActivity {
 
+    private RecyclerView recycleImgSlider;
+    private RecycleProductImageAdapter imgSliderApdater;
+    private ArrayList<String> imgList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +30,29 @@ public class ProductPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addControl();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadRecycleViewImgSlider();
+    }
+
+    private void addControl(){
+        recycleImgSlider = findViewById(R.id.recyclerProductImgSlider);
+
+    }
+    private void loadRecycleViewImgSlider(){
+        imgList = new ArrayList<>();
+        imgList.add("harringtonjacket1.png");
+        imgList.add("harringtonjacket2.png");
+        imgList.add("harringtonjacket1.png");
+        imgList.add("harringtonjacket2.png");
+        imgSliderApdater = new RecycleProductImageAdapter(imgList,getApplicationContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
+        recycleImgSlider.setLayoutManager(layoutManager);
+        recycleImgSlider.setAdapter(imgSliderApdater);
     }
 }
