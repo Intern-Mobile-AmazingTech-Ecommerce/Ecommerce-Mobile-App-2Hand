4493ba0b97180bec1ad73e13c66aae60bc57a3eb
hoangminhwer <131875396+hoangminhwer@users.noreply.github.com>
2024-08-22 17:43:14 +0700

    fix conflict1

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/SignInActivity.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Login/SignInActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
-
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
