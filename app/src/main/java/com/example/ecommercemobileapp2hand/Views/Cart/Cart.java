dabb07fea36d8c0b730e4c04f21ab5b8e23925de
khuongduong_ <134796453+khuongduong99@users.noreply.github.com>
2024-08-28 17:03:10 +0700

    commit(cart)

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Cart/Cart.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Cart/Cart.java
@@ -1,6 +1,10 @@
 package com.example.ecommercemobileapp2hand.Views.Cart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,15 +16,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.ecommercemobileapp2hand.Views.Adapters.Adapter_Cart;
 import com.example.ecommercemobileapp2hand.Models.FakeModels.Cartt;
 import com.example.ecommercemobileapp2hand.R;
+import com.example.ecommercemobileapp2hand.Views.Checkout.Checkout;
+import com.example.ecommercemobileapp2hand.Views.MainActivity;
 
 import java.util.ArrayList;
 
 public class Cart extends AppCompatActivity {
-    int image[] = {R.drawable.ao1, R.drawable.ao2};
-    String name[] = {"ao vang","ao trang"};
-    String size[] = {"Size M", "Size L"};
-    String price[] = {"$150", "$250"};
-    String color[] = {"Red","White"};
+    ImageButton back;
+    Button btncheckout;
+    //
+    int image[] = {R.drawable.ao2, R.drawable.ao1};
+    String name[] = {"Men's Harrington Jacket", "Men's Coaches Jacket"};
+    String size[] = {"Size - M","Size - M"};
+    String price[] = {"$148","$52.00"};
+    String color[] = {"Color - Lemon","Color - Black"};
     //
     ArrayList<Cartt> mylist;
     Adapter_Cart myadapter;
@@ -39,9 +48,27 @@ public class Cart extends AppCompatActivity {
         mylist = new ArrayList<>();
         for(int i = 0; i< name.length; i++)
         {
-            mylist.add(new Cartt(image[i], name[i],size[i], price[i], color[i]));
+            mylist.add(new Cartt(image[i], name[i],size[i], color[i], price[i]));
         }
         myadapter = new Adapter_Cart(Cart.this , R.layout.layout_cart,mylist);
         lv.setAdapter(myadapter);
+        //nut back
+        back = findViewById(R.id.btnBack2);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(Cart.this, MainActivity.class);
+                startActivity(myintent);
+            }
+        });
+        //nut checkout
+        btncheckout = findViewById(R.id.btnCheckout);
+        btncheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(Cart.this, Checkout.class);
+                startActivity(myintent);
+            }
+        });
     }
 }
