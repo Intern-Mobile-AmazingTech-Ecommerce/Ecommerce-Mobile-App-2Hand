bb8b175c72449f91669c3a458a2c6c17226dd559
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-09-01 00:12:18 +0700

    add cache

--- app/src/main/java/com/example/ecommercemobileapp2hand/Views/Homepage/HomeFragment.java
+++ app/src/main/java/com/example/ecommercemobileapp2hand/Views/Homepage/HomeFragment.java
@@ -20,6 +20,9 @@ import com.example.ecommercemobileapp2hand.Models.ProductCategory;
 import com.example.ecommercemobileapp2hand.R;
 import com.example.ecommercemobileapp2hand.Views.Adapters.CategoriesAdapter;
 import com.example.ecommercemobileapp2hand.Views.Adapters.ProductCardAdapter;
+import com.example.ecommercemobileapp2hand.Views.App;
+
+import org.checkerframework.checker.units.qual.A;
 
 import java.math.BigDecimal;
 import java.sql.Time;
@@ -31,8 +34,12 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
@@ -43,16 +50,18 @@ public class HomeFragment extends Fragment {
     private TextView tvSeeAll;
     private ArrayList<Product> lstPro;
     private RecyclerView recyclerViewCategories;
-    private List<ProductCategory> categoryList;
+    private ArrayList<ProductCategory> categoryList;
     private CategoriesAdapter categoriesAdapter;
 
     private RecyclerView recyclerViewNewIn;
     private ArrayList<com.example.ecommercemobileapp2hand.Models.Product> lstProNewIn;
     private ProductCardAdapter NewInAdapter;
-    private TextView tvNewInSeeAll,tvTopSellingSeeAll;
+    private TextView tvNewInSeeAll, tvTopSellingSeeAll;
     private RecyclerView recyclerViewTopSelling;
     private ArrayList<com.example.ecommercemobileapp2hand.Models.Product> lstProTopSelling;
     private ProductCardAdapter TopSellingAdapter;
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -91,14 +100,21 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        addControl(view);
+        try {
+            addControl(view);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
 
         return view;
     }
@@ -106,17 +122,26 @@ public class HomeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        loadCategoriesData();
-        loadTopSellingProductsData();
-        loadNewInProductsData();
+        try {
+            loadCategoriesData();
+            loadTopSellingProductsData();
+            loadNewInProductsData();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
         addEvent();
     }
 
-    private void addControl(View view){
+    private void addControl(View view) throws ExecutionException {
+
 
         //GenerateListPro
-        lstPro = new ArrayList<>();
-        lstPro = ProductHandler.getDataByObjectName("Men");
+        lstPro = (ArrayList<Product>) App.getCache().get("lstPro", new Callable<ArrayList<Product>>() {
+            @Override
+            public ArrayList<Product> call() throws Exception {
+               return ProductHandler.getDataByObjectName("Men");
+            }
+        });
 
         recyclerViewCategories = view.findViewById(R.id.recyclerViewCategories);
         tvSeeAll = view.findViewById(R.id.tvSeeAll);
@@ -125,67 +150,72 @@ public class HomeFragment extends Fragment {
         recyclerViewTopSelling = view.findViewById(R.id.recyclerViewTopSelling);
 
         tvNewInSeeAll = view.findViewById(R.id.tvNewInSeeAll);
-        tvTopSellingSeeAll = view .findViewById(R.id.tvTopSellingSeeAll);
+        tvTopSellingSeeAll = view.findViewById(R.id.tvTopSellingSeeAll);
 
     }
 
-    private void loadCategoriesData(){
-        categoryList = new ArrayList<>();
-        if(categoryList.size() > 5){
-            categoryList = CategoriesHandler.getData().subList(0,5);
-        }else {
-            categoryList = CategoriesHandler.getData();
-        }
-
-        categoriesAdapter = new CategoriesAdapter(categoryList,getContext(),R.layout.custom_recycle_categories_homepage);
-         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false);
-        recyclerViewCategories.setLayoutManager(layoutManager);
-       recyclerViewCategories.setAdapter(categoriesAdapter);
-
-    }
-    private void loadTopSellingProductsData(){
-        lstProTopSelling = new ArrayList<>();
-        if(lstPro.size() > 0){
-            BigDecimal minSold = new BigDecimal(0);
-            lstProTopSelling = (ArrayList<Product>) lstPro.stream().filter(product -> product.getSold().compareTo(minSold) > 0).sorted(Comparator.comparing(Product::getSold).reversed()).collect(Collectors.toList());
-            if(lstProTopSelling.size() > 5){
-                ArrayList<Product> subTopSellingList = lstProTopSelling.subList(0,5).stream().collect(Collectors.toCollection(ArrayList::new));
-                TopSellingAdapter = new ProductCardAdapter(subTopSellingList,getActivity());
-
-            }else {
-                TopSellingAdapter = new ProductCardAdapter(lstProTopSelling,getActivity());
+    private void loadCategoriesData() throws ExecutionException {
+        ArrayList<ProductCategory> categoryList = (ArrayList<ProductCategory>) App.getCache().get("categories", new Callable<ArrayList<ProductCategory>>() {
+            @Override
+            public ArrayList<ProductCategory> call() throws Exception {
+                ArrayList<ProductCategory> categories = CategoriesHandler.getData();
+                return categories.size() > 5 ? new ArrayList<>(categories.subList(0, 5)) : categories;
             }
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
-            recyclerViewTopSelling.setLayoutManager(layoutManager);
-            recyclerViewTopSelling.setAdapter(TopSellingAdapter);
-        }
+        });
+        categoriesAdapter = new CategoriesAdapter(categoryList, getContext(), R.layout.custom_recycle_categories_homepage);
+        recyclerViewCategories.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        recyclerViewCategories.setAdapter(categoriesAdapter);
+
 
     }
-    private void loadNewInProductsData(){
-        lstProNewIn = new ArrayList<>();
-        if (lstPro.size()>0){
-            LocalDateTime now = LocalDateTime.now();
-            LocalDateTime sevenDaysAgo = now.minus(7, ChronoUnit.DAYS);
-            lstProNewIn = lstPro.stream()
-                    .filter(product -> {
-                        LocalDateTime createdAt = product.getCreated_at();
-                        return createdAt.isAfter(sevenDaysAgo);
-                    })
-                    .collect(Collectors.toCollection(ArrayList::new));
-            if(lstProNewIn.size()>5){
-                ArrayList<Product> subNewInProductList = lstProNewIn.subList(0,5).stream().collect(Collectors.toCollection(ArrayList::new));
-                NewInAdapter = new ProductCardAdapter(subNewInProductList,getActivity());
-            }else {
-                NewInAdapter = new ProductCardAdapter(lstProNewIn,getActivity());
+
+    private void loadTopSellingProductsData() throws ExecutionException {
+        lstProTopSelling = (ArrayList<Product>) App.getCache().get("TopSelling", new Callable<ArrayList<Product>>() {
+            @Override
+            public ArrayList<Product> call() throws Exception {
+                    ArrayList<Product> lst = (ArrayList<Product>) lstPro.stream()
+                            .filter(product -> product.getSold().compareTo(BigDecimal.ZERO) > 0)
+                            .sorted(Comparator.comparing(Product::getSold).reversed())
+                            .collect(Collectors.toList());
+                return lstPro.size() > 0 ? new ArrayList<>(lst) : new ArrayList<>();
             }
-
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
-            recyclerViewNewIn.setLayoutManager(layoutManager);
-            recyclerViewNewIn.setAdapter(NewInAdapter);
+        });
+        if (lstProTopSelling.size() > 5) {
+            ArrayList<Product> subTopSellingList = lstProTopSelling.subList(0, 5).stream().collect(Collectors.toCollection(ArrayList::new));
+            TopSellingAdapter = new ProductCardAdapter(subTopSellingList, getActivity());
         }
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewTopSelling.setLayoutManager(layoutManager);
+        recyclerViewTopSelling.setAdapter(TopSellingAdapter);
 
     }
-    private void addEvent(){
+    private void loadNewInProductsData() throws ExecutionException {
+        lstProNewIn = (ArrayList<Product>) App.getCache().get("NewIn", new Callable<ArrayList<Product>>() {
+            @Override
+            public ArrayList<Product> call() throws Exception {
+                if (lstPro.size() > 0) {
+                    LocalDateTime now = LocalDateTime.now();
+                    LocalDateTime sevenDaysAgo = now.minus(7, ChronoUnit.DAYS);
+                    return lstPro.stream()
+                            .filter(product -> product.getCreated_at().isAfter(sevenDaysAgo))
+                            .collect(Collectors.toCollection(ArrayList::new));
+                }
+                return new ArrayList<>();
+            }
+        });
+
+        if (lstProNewIn.size() > 5) {
+            ArrayList<Product> subNewInProductList = lstProNewIn.subList(0, 5).stream().collect(Collectors.toCollection(ArrayList::new));
+            NewInAdapter = new ProductCardAdapter(subNewInProductList, getActivity());
+        }
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewNewIn.setLayoutManager(layoutManager);
+        recyclerViewNewIn.setAdapter(NewInAdapter);
+
+    }
+
+    private void addEvent() {
         tvSeeAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -200,7 +230,7 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), CategoryProductActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putParcelableArrayList("TopSellingList",lstProTopSelling);
+                bundle.putParcelableArrayList("TopSellingList", lstProTopSelling);
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
@@ -211,7 +241,7 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), CategoryProductActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putParcelableArrayList("NewInList",lstProNewIn);
+                bundle.putParcelableArrayList("NewInList", lstProNewIn);
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
